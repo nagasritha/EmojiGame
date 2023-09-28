528651046692b51c0d778604ee437076a0a497ab
nagasritha <eernagasritha@gmail.com>
2023-09-28 21:56:33 +0530

    6th commit

--- src/components/EmojiGame/index.js
+++ src/components/EmojiGame/index.js
@@ -1,15 +1,27 @@
 import {Component} from 'react'
 import EmojiCard from '../EmojiCard'
+import NavBar from '../NavBar'
 import './index.css'
 
 class EmojiGame extends Component{
     state={
         displayList:this.props.emojisList,
+        clickedEmojisList: [],
+        isGameInProgress: true,
+        topScore: 0,
     }
     
     clickEmoji=(id)=>{
       const {emojisList}=this.props
-      this.setState({displayList:emojisList.sort(()=>Math.random()-0.5)})
+      const {clickedEmojisList}=this.state
+      const shuffledList= emojisList.sort(()=>Math.random()-0.5)
+
+      const bool=clickedEmojisList.includes(id)
+      console.log(bool)
+      if(!bool){
+         return this.setState(prevState=>({clickedEmojisList:[...prevState.clickedEmojisList,id],displayList:shuffledList}))
+      } 
+      return this.setState(prevState=>({isGameInProgress:!prevState.isGameInProgress}))     
     }
 
     renderEmojisList=()=>{
@@ -20,12 +32,20 @@ class EmojiGame extends Component{
         </div>
       )
     }
+
+    resultView=()=><h1>result</h1>
     
     render(){
+        const {isGameInProgress,clickedEmojisList,topScore}=this.state
      return (
         <div className='app-container'>
+             <NavBar
+          currentScore={clickedEmojisList.length}
+          isGameInProgress={isGameInProgress}
+          topScore={topScore}
+        />
             <div className='enmoji-game-body'>
-               {this.renderEmojisList()}          
+               {isGameInProgress ? this.renderEmojisList() : this.resultView()}          
             </div>
         </div>
      )

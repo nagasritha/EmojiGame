7e3aa2a5498163f33213d467923bea8c5a96b9fe
nagasritha <eernagasritha@gmail.com>
2023-09-28 21:01:12 +0530

    third commmit

--- src/components/EmojiCard/index.js
+++ src/components/EmojiCard/index.js
@@ -1,16 +1,12 @@
 import './index.css'
 
 const EmojiCard = props => {
-  const {emojiDetails, clickEmoji} = props
-  const {id, emojiName, emojiUrl} = emojiDetails
-
-  const onClickEmojiCard = () => {
-    clickEmoji(id)
-  }
+  const {emojiDetails} = props
+  const {emojiName, emojiUrl} = emojiDetails
 
   return (
     <li className="emoji-item">
-      <button type="button" className="emoji-btn" onClick={onClickEmojiCard}>
+      <button type="button" className="emoji-btn">
         <img className="emoji-icon" src={emojiUrl} alt={emojiName} />
       </button>
     </li>

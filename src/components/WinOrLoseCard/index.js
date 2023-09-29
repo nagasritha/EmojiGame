957dd3e05dd09de6b2021373aedff8e34ebc93e7
nagasritha <eernagasritha@gmail.com>
2023-09-29 07:47:37 +0530

    last commit

--- src/components/WinOrLoseCard/index.js
+++ src/components/WinOrLoseCard/index.js
@@ -9,11 +9,26 @@ const WinOrLoseCard = props => {
   const [play2]=useSound(failureMusic)
   isWon ? play1() : play2()
   const playAgain=()=>onClickPlayAgain(score)
+  const gameStatus=isWon ? 'You Won' : 'You Lose'
+  const scoreLabel=isWon ? 'Best Score' : 'Score'
   return (
    <div className='align'>
      <div className="win-or-lose-card">
-       <h1>hello world</h1>
-       <button type='button' className='play-again-button' onClick={playAgain}>play again</button>
+     <div className="details-section">
+        <h1 className="game-status">{gameStatus}</h1>
+        <p className="current-score-label">{scoreLabel}</p>
+        <p className="current-score-value">{score}/12</p>
+        <button
+          type="button"
+          className="play-again-button"
+          onClick={playAgain}
+        >
+          Play Again
+        </button>
+      </div>
+      <div className="image-section">
+        <img className="win-or-lose-image" src={isWon ? 'https://assets.ccbp.in/frontend/react-js/won-game-img.png' : 'https://assets.ccbp.in/frontend/react-js/lose-game-img.png'} alt="win or lose" />
+      </div>
      </div>
    </div>
    

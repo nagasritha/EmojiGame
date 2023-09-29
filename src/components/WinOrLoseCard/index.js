4b478f02eac26b9b258276ceb3b00bba1e538a6a
nagasritha <eernagasritha@gmail.com>
2023-09-29 07:16:21 +0530

    11th commit

--- src/components/WinOrLoseCard/index.js
+++ src/components/WinOrLoseCard/index.js
@@ -1,8 +1,13 @@
-
+import useSound from 'use-sound'
+import successMusic from '../../success.wav'
+import failureMusic from '../../GameFailure.wav'
 import './index.css'
 
-const WinOrLoseCard =() => {
-
+const WinOrLoseCard = props => {
+  const {isWon}=props
+  const [play1]=useSound(successMusic)
+  const [play2]=useSound(failureMusic)
+  isWon ? play1() : play2()
   return (
    <div className='align'>
      <div className="win-or-lose-card">
